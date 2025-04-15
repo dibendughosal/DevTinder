0b4375dc2e48342e4c5af5eb8bd7089ad1123737
Dibendu Ghosal <128916258+dibendughosal@users.noreply.github.com>
2025-04-15 15:29:41 +0530

    Add User Schema

--- src/app.js
+++ src/app.js
@@ -1,7 +1,30 @@
 const express = require('express');
-
+const connectDB = require("./database/db");
 const app = express();
-app.get("/home", (req,res)=> res.send("<h1>Hey folks!</h1>"));
-app.use("/", (req, res)=> res.send("Hello from dashboard"))
-app.listen(7777, ()=> console.log("Server running at 7777")
-);
+const User = require('./models/userSchema');
+
+app.post("/signup", async (req,res) => {
+    const user = new User({
+        firstName: "Dibendu",
+        lastName: "Ghosal",
+        email: "dibendughosal@gmail.com",
+        password: "Dibyo2002"
+    });
+
+    try{
+        await user.save()
+        res.send("User added Successfully")
+    }
+    catch(err){
+        res.status(401).send("Error", err.message)
+    }
+});
+
+connectDB()
+.then(() => {
+    console.log("Database Connection Successfully");
+    app.listen(7777, () => console.log("Server running at 7777"));
+})
+.catch((err) => {
+    console.error("Error in Connection", err.message);
+})
---
+++ src/database/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const connectDB = async(req, res) => {
+    try{
+        await mongoose.connect("mongodb://localhost:27017/devTinder")
+        res.send("database connection Successfully");
+    }
+    catch(err){
+        console.error("Error...");
+    }
+}
+module.exports = connectDB;
